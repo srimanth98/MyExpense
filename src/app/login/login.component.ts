566035a9e7fcf9c5809edcec85b8ba5930721cc5
Srimanth Agastyaraju <srimanth98@gmail.com>
2021-01-09 23:07:18 +0530

    Mock Login; Light and Dark theme css files

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  
+  hide:boolean;
+  loginForm = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+    ]),
+  });
 
-  constructor() { }
+  constructor() {
+    this.hide = true;
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit(){
+    console.log(this.loginForm.value);
+    console.log("Submitted");    
+  }
 }
